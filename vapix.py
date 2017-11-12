5feeaf6f105a4161a26f88c45e7bbe952c2b4912
Z <kerenyi.nagy.zoltan@gmail.com>
2017-11-12 12:43:08 +0100

    Update vapix.py

--- vapix.py
+++ vapix.py
@@ -1,16 +1,40 @@
 import requests
 
 class Axis:
-
-    def __init__(self, id='default', ip='192.168.0.90', username='root', password='root'):
-        self.id = ID
+    """ Describes an Axis camera object
+    initialize it with the default arguments
+    after resetting the camera """
+    def __init__(self, ip='192.168.0.90', username='root', password='root'):
         self.ip = ip
         self.username = username
         self.password = password
+        self.base_url = 'http://{}:{}@{}/axis-cgi/'.format(username, password, ip)
+
+    def get_VAPIX_version(self):
+        """Returns the version of Axis VAPIX API supported by the camera"""
+        try:
+            r = requests.get(self.base_url + 'param.cgi?action=list&group=Properties.API.HTTP.Version')
+            if r.status_code == 200:
+                r = str(r.content.decode('utf-8'))
+                print(r)
+            else:
+                print(r.status_code)
+
+        except:
+            print('Error')
+            pass
+
 
     def restart(self):
-        # API: http://<servername>/axis-cgi/admin/restart.cgi
+        """ Restarts the Axis camera """
         try:
-            requests.get("http://" + self.username + ":" + self.password + "@" + self.ip + "/axis-cgi/admin/restart.cgi")
+            r = requests.get(self.base_url + '/admin/restart.cgi')
+            print('Restaring ' + self.ip)
+            print('Return status: ' + str(r.status_code))
+            if r.status_code == 200:
+                print('Axis camera has been restarted succesfully')
+                return r.status_code
+            else:
+                return r.status_code
         except:
             pass
